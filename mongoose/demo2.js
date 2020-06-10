7a928db798607fb049d2b9aaebb97407019d2e1c
hs312720804@qq.com <hs312720804@qq.com>
2020-06-10 15:11:47 +0800

    hs

--- mongoose/demo2.js
+++ mongoose/demo2.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
-// 连接 MongoDB 数据库 
+// 1、连接 MongoDB 数据库 
 mongoose.connect('mongodb://localhost/test');
 
-var mongoose = require('mongoose');
+
 var Schema = mongoose.Schema;
 
-// 设计集合结构（表结构）
+// 2、设计集合结构（表结构）
 // 字段名称就是表结构中的属性名称
-// 约束的耳目的是为了保证数据的完整性，不要有脏数组
+// 约束的目的是为了保证数据的完整性，不要有脏数组
 // var blogSchema = new Schema({
 //   title:  String,
 //   author: String,
@@ -38,5 +38,27 @@ var userSchema = new Schema({
 
 // 3、将文档结构发布为模型
 // mongoose.model 方法就是用来将一个架构发布为 model
-// 第一个参数：出入一个大写名词单数字符串用来标书你的数据库名词
-// mongoose会
+// 第一个参数：传入一个大写名词单数字符串用来标书你的数据库名词
+//             mongoose会自动将大写名词的字符串生成 小写复数 的集合名称
+//             例如这里的 User 最终会变成 users 集合名称
+// 第二个参数：架构 Schema
+
+// 返回值：模型构造函数
+var User = mongoose.model('User', userSchema)
+
+
+// 4. 当我们有了模型构造函数之后，就可以使用这个构造函数对 User 中的数据为所欲为了（增删改查）
+var admin = new User({
+    username: 'admin',
+    password: '123456',
+    email: '123@qq.com'
+})
+
+admin.save(function (err, ret) {
+    if (err) {
+        console.log('请求失败')
+    } else {
+        console.log('保存成功')
+        console.log(ret)
+    }
+})
